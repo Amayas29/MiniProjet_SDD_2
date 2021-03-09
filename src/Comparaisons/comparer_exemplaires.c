e9e608a098fc45ba1517577acafe2cfd6149f3bb
Amayas29 <amayas29sd@gmail.com>
2021-03-09 21:52:16 +0100

    Add comments to comparer_exemplaires.c

--- src/Comparaisons/comparer_exemplaires.c
+++ src/Comparaisons/comparer_exemplaires.c
@@ -11,6 +11,7 @@
 
 int main(void) {
 
+    // On ouvre le fichier ou enregistrer les temps
     FILE *file_exemplaires = fopen("file_exemplaires.log", "w");
     if(!file_exemplaires) {
         print_probleme("Erreur de chargement");
@@ -28,8 +29,11 @@ int main(void) {
     Biblio *liste_lc = NULL;
     LivreH *liste_h = NULL;
 
+    // On varie la taille de 1000 jusqu'à NB_ITERATION (par defaut c'est 50000)
     for(int taille = 1000; taille <= NB_ITERATION; taille += 1000) {
 
+        // On charge les bibliotheques
+        
         biblio_lc = charger_n_entrees_lc("../../ressources/GdeBiblio.txt", taille);
         if(!biblio_lc) {
             print_probleme("Erreur de chargement");
@@ -45,6 +49,7 @@ int main(void) {
             return 1;
         }
 
+        // On lance les recherches d'exemplaires et on calcule les temps pour chaque verision
         temps_initial = clock();
         liste_lc = rechercher_exemplaires_lc(biblio_lc);
         temps_final = clock();
@@ -57,6 +62,7 @@ int main(void) {
 
         fprintf(file_exemplaires, "%d %f %f\n", taille, temps_cpu_lc, temps_cpu_h);
 
+        // On libere la memoire alloue à la fin du tour pour passer au suivant
         liberer_livres_h(liste_h);
         liberer_biblio_lc(liste_lc);
         liberer_biblio_lc(biblio_lc);
