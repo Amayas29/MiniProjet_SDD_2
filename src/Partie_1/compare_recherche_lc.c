b2e4f9c86e44ce2760d6b0a0de0a2cff26d687d6
Amayas29 <amayas29sd@gmail.com>
2021-03-09 01:47:18 +0100

    Compars

--- src/Partie_1/compare_recherche_lc.c
+++ src/Partie_1/compare_recherche_lc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 #include "biblioLC.h"
 #include "entreeSortieLC.h"
-
 #include "../commun.h"
 
 
@@ -15,45 +15,52 @@ int main(void) {
     clock_t temps_final;
     double temps_cpu_lc;
     
-    int numeros[] = {1, 25, 198, 23, 49, 190, 356, 897, 2, 10, 1029, 1893, 26387, 38942, 18937, 
-                            3648, 9894, 7848, 4765, 9384};
+    int numeros[] = {
+           1,   25,  198,     23,   49,
+         190,  356,  897,      2,    10,
+        1029, 1893, 26387, 38942, 18937,
+        3648, 9894,  7848,  4765,  9384
+    };
 
-    printf("\n\n____________________Recherche par numero____________________\n\n");
-    for (int i = 0; i < 20; i++) {   
+    printf("\n\n____________________ Recherche par numéro ____________________\n\n");
+    
+    for (int i = 0; i < 20; i++) {
         temps_initial = clock();
         rechercher_biblio_numero(biblio_lc,numeros[i]);
         temps_final = clock();
         temps_cpu_lc = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;
-        
-
-        printf(" >> Recherche par numero LC = %f  \n\n",temps_cpu_lc);
+        printf(" >> Recherche par le numéro : %f\n",temps_cpu_lc);
     }
 
-    printf("\n\n____________________Recherche par livre____________________\n\n");
+    // ------------------------------------------------------------------------------
 
+    printf("\n\n____________________ Recherche par livre ____________________\n\n");
 
-    char *titres[] = {"CPQIUDQPNYLN", "FWKHOPKMCOQHNWNKUE", "BUDPEHDMF", "KMLNOZJKPQPXR0", "IOHORDTQKVWC", 
-    "SOTBUXEHOYCHIFS", "LNQIIEZHIBHAIV", "TYXBGJZUMLLPXD0", "FLDE", "SKLUZUWS", "FGHVUVHEAJL", "LKEJOBEZJK", 
-    "RGHBGUZJDPZKJE", "MPOOPJAPONAA", "LMAATYCWJA", "OIHPS", "ALZNPMKLNS", "POJOIBAAXA", "KHJBVIUAW", "WMLOPAWPO" };
+    char *titres[] = {
+           "CPQIUDQPNYLN", "FWKHOPKMCOQHNWNKUE",       "BUDPEHDMF", "KMLNOZJKPQPXR0", "IOHORDTQKVWC", 
+        "SOTBUXEHOYCHIFS",     "LNQIIEZHIBHAIV", "TYXBGJZUMLLPXD0",           "FLDE",     "SKLUZUWS",
+            "FGHVUVHEAJL",         "LKEJOBEZJK",  "RGHBGUZJDPZKJE",   "MPOOPJAPONAA",   "LMAATYCWJA",
+                  "OIHPS",         "ALZNPMKLNS",      "POJOIBAAXA",      "KHJBVIUAW", "WMLOPAWPO"
+    };
 
     for (int i = 0; i < 20; i++) {   
         temps_initial = clock();
         rechercher_biblio_titre(biblio_lc,titres[i]);
         temps_final = clock();
         temps_cpu_lc = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;
-        
-
-        printf(" >> Recherche par titre LC = %f  \n\n",temps_cpu_lc);
+        printf(" >> Recherche par le titre : %f\n",temps_cpu_lc);
     }
-    
-    printf("\n\n____________________Recherche par auteur____________________\n\n");
-    
-    char *auteurs[] = {"zrlkosnu", "qnrajro", "efsarcbynecd", "wxwtwheibqv", "aijvwcyau",
-     "orxxvrwctdsn", "ffewggftw", "gwofojj", "selsftsedu", "nxcxmjcxoh", "azdzlopxa", "wxcoizaaze",
-      "osqnoazc", "aqbiubaiaz", "cqsyubrz", "mazopwabal", "bczvhjabx", "nbwgycaz", "amayas", "hamid"};
 
-    
+    // ------------------------------------------------------------------------------
 
+    printf("\n\n____________________ Recherche par auteur ____________________\n\n");
+    
+    char *auteurs[] = {
+            "zrlkosnu",    "qnrajro", "efsarcbynecd", "wxwtwheibqv",  "aijvwcyau",
+        "orxxvrwctdsn",  "ffewggftw",      "gwofojj",  "selsftsedu", "nxcxmjcxoh",
+           "azdzlopxa", "wxcoizaaze",     "osqnoazc",  "aqbiubaiaz",   "cqsyubrz",
+          "mazopwabal",  "bczvhjabx",     "nbwgycaz",      "amayas",      "hamid"
+    };
 
     Biblio *biblio_search = NULL;
     for (int i = 0; i < 20; i++) {   
@@ -62,21 +69,9 @@ int main(void) {
         temps_final = clock();
         temps_cpu_lc = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;      
         liberer_biblio(biblio_search);
-
-      
-        printf(" >> Recherche par auteurs LC = %f  \n\n",temps_cpu_lc);
+        printf(" >> Recherche par l'auteur : %f\n",temps_cpu_lc);
     }
-    
-    
-
 
     liberer_biblio(biblio_lc);
-
-  return 0;
+    return 0;
 }
-
-
-
-
-
-
--- src/Partie_2/compare_recherche_h.c
+++ src/Partie_2/compare_recherche_h.c
@@ -1,66 +1,74 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "./Partie_2/biblioH.h"
-#include "./Partie_2/entreeSortieHach.h"
+#include "biblioH.h"
+#include "entreeSortieHach.h"
 
 int main(void) {
 
-    BiblioH *biblio_h = charger_n_entrees("ressources/GdeBiblio.txt",1000);
+    BiblioH *biblio_h = charger_n_entrees("ressources/GdeBiblio.txt", 1000);
 
     clock_t temps_initial;
     clock_t temps_final;
- 
     double temps_cpu_h;
-    int numeros[] = {1, 25, 198, 23, 49, 190, 356, 897, 2, 10, 1029, 1893, 26387, 38942, 18937, 
-                            3648, 9894, 7848, 4765, 9384};
 
-    printf("\n\n____________________Recherche par numero____________________\n\n");
+    int numeros[] = {
+           1,   25,  198,     23,   49,
+         190,  356,  897,      2,    10,
+        1029, 1893, 26387, 38942, 18937,
+        3648, 9894,  7848,  4765,  9384
+    };
+
+    printf("\n\n____________________ Recherche par numéro ____________________\n\n");
+
     for (int i = 0; i < 20; i++) {   
-        
         temps_initial = clock();
         rechercher_biblio_numero(biblio_h,numeros[i]);
         temps_final = clock();
         temps_cpu_h = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;
-
-        printf(" >> Recherche par numero  H = %f \n\n",temps_cpu_h);
+        printf(" >> Recherche par le numéro : %f\n",temps_cpu_h);
     }
 
-    printf("\n\n____________________Recherche par livre____________________\n\n");
+    // ------------------------------------------------------------------------------
 
+    printf("\n\n____________________ Recherche par livre ____________________\n\n");
 
-    char *titres[] = {"CPQIUDQPNYLN", "FWKHOPKMCOQHNWNKUE", "BUDPEHDMF", "KMLNOZJKPQPXR0", "IOHORDTQKVWC", 
-    "SOTBUXEHOYCHIFS", "LNQIIEZHIBHAIV", "TYXBGJZUMLLPXD0", "FLDE", "SKLUZUWS", "FGHVUVHEAJL", "LKEJOBEZJK", 
-    "RGHBGUZJDPZKJE", "MPOOPJAPONAA", "LMAATYCWJA", "OIHPS", "ALZNPMKLNS", "POJOIBAAXA", "KHJBVIUAW", "WMLOPAWPO" };
+    char *titres[] = {
+           "CPQIUDQPNYLN", "FWKHOPKMCOQHNWNKUE",       "BUDPEHDMF", "KMLNOZJKPQPXR0", "IOHORDTQKVWC", 
+        "SOTBUXEHOYCHIFS",     "LNQIIEZHIBHAIV", "TYXBGJZUMLLPXD0",           "FLDE",     "SKLUZUWS",
+            "FGHVUVHEAJL",         "LKEJOBEZJK",  "RGHBGUZJDPZKJE",   "MPOOPJAPONAA",   "LMAATYCWJA",
+                  "OIHPS",         "ALZNPMKLNS",      "POJOIBAAXA",      "KHJBVIUAW", "WMLOPAWPO"
+    };
 
     for (int i = 0; i < 20; i++) {   
-    
         temps_initial = clock();
         rechercher_biblio_titre(biblio_h,titres[i]);
         temps_final = clock();
         temps_cpu_h = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;
-
-        printf(" >> Recherche par titre  H = %f \n\n",temps_cpu_h);
+        printf(" >> Recherche par le titre %f\n",temps_cpu_h);
     }
-    printf("\n\n____________________Recherche par auteur____________________\n\n");
+
+    // ------------------------------------------------------------------------------
+
+    printf("\n\n____________________ Recherche par auteur ____________________\n\n");
     
-    char *auteurs[] = {"zrlkosnu", "qnrajro", "efsarcbynecd", "wxwtwheibqv", "aijvwcyau",
-     "orxxvrwctdsn", "ffewggftw", "gwofojj", "selsftsedu", "nxcxmjcxoh", "azdzlopxa", "wxcoizaaze",
-      "osqnoazc", "aqbiubaiaz", "cqsyubrz", "mazopwabal", "bczvhjabx", "nbwgycaz", "amayas", "hamid"};
+    char *auteurs[] = {
+            "zrlkosnu",    "qnrajro", "efsarcbynecd", "wxwtwheibqv",  "aijvwcyau",
+        "orxxvrwctdsn",  "ffewggftw",      "gwofojj",  "selsftsedu", "nxcxmjcxoh",
+           "azdzlopxa", "wxcoizaaze",     "osqnoazc",  "aqbiubaiaz",   "cqsyubrz",
+          "mazopwabal",  "bczvhjabx",     "nbwgycaz",      "amayas",      "hamid"
+    };
+
     LivreH *livre_liste_h = NULL;
     for (int i = 0; i < 20; i++) {   
-  
         temps_initial = clock();
         livre_liste_h = rechercher_biblio_auteur(biblio_h,auteurs[i]);
         temps_final = clock();
         temps_cpu_h = ((double) (temps_final - temps_initial)) / CLOCKS_PER_SEC;
         liberer_livres(livre_liste_h);
-
-        printf(" >> Recherche par auteurs  H = %f \n\n",temps_cpu_h);
+        printf(" >> Recherche par l'auteur : %f\n",temps_cpu_h);
     }
-    
-    
-    liberer_biblio(biblio_h);
 
-  return 0;
+    liberer_biblio(biblio_h);
+    return 0;
 }
